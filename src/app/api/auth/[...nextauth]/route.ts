a3a73997f98cd286e4320341e76845ef2c8bfb76
AYOMIDE-LIADI <167301347+AYOMIDE-LIADI@users.noreply.github.com>
2025-08-27 11:22:44 +0100

    another change

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,15 +5,15 @@ import GoogleProvider from "next-auth/providers/google"
 const handler = NextAuth({
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.NEXT_GITHUB_ID!,
+      clientSecret: process.env.NEXT_GITHUB_SECRET!,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.NEXT_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.NEXT_GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_NEXTAUTH_SECRET,
 })
 
 export { handler as GET, handler as POST }
